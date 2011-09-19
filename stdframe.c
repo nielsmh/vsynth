e8665fa65544f041211ec27f7d912791cff54e17
Niels Martin Hansen <nielsm@indvikleren.dk>
2011-09-19 16:24:33 +0200

    Add some pixel formats and the ability to allocate mono and RGB frames

--- stdframe.c
+++ stdframe.c
@@ -30,10 +30,43 @@ struct StandardFrameVirtual stdframe_vtable = {
 };
 
 
-VSYNTH_API(StandardFrame) Stdframe_New(int pixfmt, size_t width, size_t height)
+VSYNTH_API(StandardFrame) Stdframe_New(enum StdframePixelFormat pixfmt, size_t width, size_t height)
 {
 	StandardFrame frame = (StandardFrame)malloc(sizeof(struct StandardFrame));
-	// todo: alloc pixels
+
+	frame->pixfmt = pixfmt;
+	frame->base.width = width;
+	frame->base.height = height;
+
+	switch (pixfmt)
+	{
+	case STDPIXFMT_MONO8:
+		frame->data_baseptr = malloc(width*height);
+		frame->data[0] = frame->data_baseptr;
+		frame->stride[0] = width;
+		break;
+	case STDPIXFMT_MONO16:
+		frame->data_baseptr = malloc(width*height*2);
+		frame->data[0] = frame->data_baseptr;
+		frame->stride[0] = width*2;
+		break;
+	case STDPIXFMT_XRGB8:
+	case STDPIXFMT_ARGB8:
+		frame->data_baseptr = malloc(width*height*4);
+		frame->data[0] = frame->data_baseptr;
+		frame->stride[0] = width*4;
+		break;
+	case STDPIXFMT_XRGB16:
+	case STDPIXFMT_ARGB16:
+		frame->data_baseptr = malloc(width*height*8);
+		frame->data[0] = frame->data_baseptr;
+		frame->stride[0] = width*8;
+		break;
+	default:
+		// lol
+		break;
+	}
+
 	return frame;
 }
 
--- stdframe.h
+++ stdframe.h
@@ -3,6 +3,57 @@
 HEADER_START
 
 
+enum StdframePixelFormat {
+	// one channel, uint8_t pixels, gamma corrected, stored in plane 0, other planes unused
+	STDPIXFMT_MONO8,
+	// one channel, uint16_t pixels, linear gamma, stored in plane 0, other planes unused
+	STDPIXFMT_MONO16,
+	// XRGB, packed uint8_t channels, gamma corrected, stored in plane 0, other planes unused
+	STDPIXFMT_XRGB8,
+	// ARGB, packed uint8_t channels, gamma corrected, stored in plane 0, other planes unused
+	STDPIXFMT_ARGB8,
+	// XRGB, packed uint16_t channels, linear gamma, stored in plane 0, other planes unused
+	STDPIXFMT_XRGB16,
+	// ARGB, packed uint16_t channels, linear gamma, stored in plane 0, other planes unused
+	STDPIXFMT_ARGB16,
+	// YCrCb 4:4:4, uint8_t pixels, gamma corrected
+	// plane 0 is Y, full resolution
+	// plane 1 is Cr, full resolution
+	// plane 2 is Cb, full resolution
+	// plane 3 is unused
+	STDPIXFMT_YCrCb8_444,
+	// same as YCrCb8_444, but plane 3 is alpha at full resolution
+	STDPIXFMT_YCrCbA8_444,
+	// same as YCrCb8_444, but with uint16_t pixels (still gamma corrected)
+	STDPIXFMT_YCrCb16_444,
+	// same as YCrCb16_444, but plane 3 is alpha at full resolution
+	STDPIXFMT_YCrCbA16_444,
+	// YCrCb 4:2:2, uint8_t pixels, gamma corrected
+	// plane 0 is Y, full resolution
+	// plane 1 is Cr, half resolution in width, full in height
+	// plane 2 is Cb, half resolution in width, full in height
+	// plane 3 is unused
+	STDPIXFMT_YCrCb8_422,
+	// same as YCrCb8_422, but plane 3 is alpha at full resolution
+	STDPIXFMT_YCrCbA8_422,
+	// same as YCrCb8_422, but with uint16_t pixels (still gamma corrected)
+	STDPIXFMT_YCrCb16_422,
+	// same as YCrCb16_422, but plane 3 is alpha at full resolution
+	STDPIXFMT_YCrCbA16_422,
+	// YCrCb 4:2:0, uint8_t pixels, gamma corrected
+	// plane 0 is Y, full resolution
+	// plane 1 is Cr, half resolution in width, half resolution in height
+	// plane 2 is Cb, half resolution in width, half resolution in height
+	// plane 3 is unused
+	STDPIXFMT_YCrCb8_420,
+	// same as YCrCb8_420, but plane 3 is alpha at full resolution
+	STDPIXFMT_YCrCbA8_420,
+	// same as YCrCb8_420, but with uint16_t pixels (still gamma corrected)
+	STDPIXFMT_YCrCb16_420,
+	// same as YCrCb16_420, but plane 3 is alpha at full resolution
+	STDPIXFMT_YCrCbA16_420,
+};
+
 struct StandardFrameVirtual {
 	struct FrameVirtual base;
 };
@@ -15,12 +66,14 @@ struct StandardFrame {
 	size_t stride[4];
 	void *data[4];
 
+	enum StdframePixelFormat pixfmt;
+
 	void *data_baseptr;
 };
 typedef struct StandardFrame *StandardFrame;
 
 
-VSYNTH_API(StandardFrame) Stdframe_New(int pixfmt, size_t width, size_t height);
+VSYNTH_API(StandardFrame) Stdframe_New(enum StdframePixelFormat pixfmt, size_t width, size_t height);
 VSYNTH_API(StandardFrame) Stdframe_Get(Frame frame);
 
 
